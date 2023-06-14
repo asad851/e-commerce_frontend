90468873576729893002d9bba5f1140bfbfd4773
asad851 <siddiquiasad851@gmail.com>
2023-06-14 14:12:21 +0530

    e-com

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,8 +6,8 @@ import logo from "../assets/logo.svg";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import HoverComponent from "./HoverComponent";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { VscChromeClose } from "react-icons/vsc";
-import { Drawer } from "flowbite";
+import { RxCaretRight } from "react-icons/rx";
+
 export default function Header() {
   const [opaque, setOpaque] = useState("hidden");
   const [item, setItem] = useState(null);
@@ -83,106 +83,79 @@ export default function Header() {
 
         <div className=" items-center flex min-[1000px]:hidden w-screen justify-between px-[25px] box-border ">
           <img className=" cursor-pointer " src={logo} alt="logo" />
-          {!showSlider ? (
+          <div className="flex gap-[25px] items-centert">
+            <BsSearch className=" cursor-pointer" />
             <RxHamburgerMenu
-              className="text-xl font-bold"
+              className="text-xl font-bold cursor-pointer"
               onClick={() => setShowSlider(true)}
             />
-          ) : (
-            <VscChromeClose
-              className="text-xl font-bold"
+          </div>
+          <div
+            id="drawer-right-example"
+            class={`fixed top-0 ${
+              showSlider ? "right-[0px] " : "right-[-500px]"
+            } z-40 p-4 overflow-y-auto transition-[right] duration-500 w-[100%] min-[400px]:w-[40%] h-screen bg-white ease-out shadow-xl  box-border`}
+            tabindex="-1"
+            aria-labelledby="drawer-right-label"
+          >
+            
+            <button
+              type="button"
+              data-drawer-hide="drawer-right-example"
+              aria-controls="drawer-right-example"
+              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-4 right-3 inline-flex items-center "
               onClick={() => setShowSlider(false)}
-            />
-          )}
-          
-              
-              <div
-                id="drawer-right-example"
-                class={`fixed top-0 ${showSlider?"right-[240px]":"right-0"} z-40 h-[90%] p-4 overflow-y-auto transition-[right] translate-x-full w-[60%] bg
-                 shadow-[0_8px_20px_-16px_rgba(0,0,0,0.3)]`}
-                tabindex="-1"
-                aria-labelledby="drawer-right-label"
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <h5
-                  id="drawer-right-label"
-                  class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"
-                >
-                  <svg
-                    class="w-5 h-5 mr-2"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  Right drawer
-                </h5>
-                <button
-                  type="button"
-                  data-drawer-hide="drawer-right-example"
-                  aria-controls="drawer-right-example"
-                  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  <svg
-                    aria-hidden="true"
-                    class="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  <span class="sr-only">Close menu</span>
-                </button>
-                <p class="mb-6 text-sm text-gray-500 dark:text-gray-400">
-                  Supercharge your hiring by taking advantage of our{" "}
-                  <a
-                    href="#"
-                    class="text-blue-600 underline font-medium dark:text-blue-500 hover:no-underline"
-                  >
-                    limited-time sale
-                  </a>{" "}
-                  for Flowbite Docs + Job Board. Unlimited access to over 190K
-                  top-ranked candidates and the #1 design job board.
-                </p>
-                <div class="grid grid-cols-2 gap-4">
-                  <a
-                    href="#"
-                    class="px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                  >
-                    Learn more
-                  </a>
-                  <a
-                    href="#"
-                    class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                  >
-                    Get access{" "}
-                    <svg
-                      class="w-4 h-4 ml-2"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </a>
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span class="sr-only">Close menu</span>
+            </button>
+            <div className="flex flex-col w-full gap-[100px] text-[20px] font-bold mt-[50px] px-[50px] box-border">
+              <ul className="flex flex-col w-full  gap-[30px] text-[20px] items-center overflow-hidden  text-[rgba(0,0,0,0.7)]     box-border relative  ">
+                {NavArray.map((data, index) => {
+                  return (
+                    <>
+                      <div className="flex items-center justify-between gap-[15px] w-full cursor-pointer hover:bg-slate-200 rounded-[5px] px-[10px] ">
+                        <li
+                          key={index}
+                          className={`  `}
+                          onMouseOver={() => {
+                            handleMouseOver(data);
+                          }}
+                          onMouseLeave={handleMouseLeave}
+                        >
+                          {data}
+                        </li>
+                        <RxCaretRight />
+                      </div>
+                    </>
+                  );
+                })}
+                
+              </ul>
+                <div className="flex flex-col gap-[10px] text-slate-100">
+                <div className="w-full flex items-center cursor-pointer   bg-gradient-to-br from-pink-500 to-orange-400 rounded-md justify-center p-[5px]">
+                  <p>Account</p>
+                  <MdPermIdentity className="text-[25px]" />
                 </div>
-              </div>
-           
+                <div className="w-full flex items-center cursor-pointer  bg-gradient-to-br from-pink-500 to-orange-400 rounded-md justify-center p-[5px]">
+                  <p>Cart</p>
+                  <AiOutlineShoppingCart className="text-[22px]" />
+                </div>
+                </div>
+            </div>
+          </div>
         </div>
       </nav>
     </>
