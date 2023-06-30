c4640e37bcf6617542af9e9ad3703ac5b50cde24
Asad Ahmed Siddiqui <118800128+asad851@users.noreply.github.com>
2023-06-30 12:34:18 +0000

    e-com

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -32,6 +32,10 @@ function PaymentPage () {
         <div className='flex gap-2 w-full'>
         
         </div>
+        <button onClick={()=>navigate(`/Payment`)} className="text-white mt-8 mb-5  bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+            >
+             Proceed to Pay {total} Rs
+            </button>
         </div>
     </div>
   )
